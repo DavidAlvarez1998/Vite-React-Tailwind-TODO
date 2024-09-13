c3324fefde784d32cd9a1337a2a42905b8044d29
David Alvarez <adjuan123@gmail.com>
2024-09-13 01:27:00 -0500

    a

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,7 @@ import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, deleteTodo, updateTodo }) => {
     return (
-        <div className='rounded-t-md bg-white mt-8'>
+        <div className='rounded-t-md bg-white mt-8 overflow-hidden dark:bg-gray-800'>
             {todos.map((todo) => (
                 <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} updateTodo={updateTodo} />
             ))}
